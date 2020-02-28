dc44ba071c90915fca867abdd4b1a8c0220224c8
leon-lyceum <57870714+leon-lyceum@users.noreply.github.com>
2020-02-28 18:31:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ LON = 37.530887
 LAT = 55.703118
 
 w, h = 600, 450
-rate = 64
+rate = 32
 pygame.init()
 clock = pygame.time.Clock()
 win = pygame.display.set_mode((w, h))
@@ -24,7 +24,6 @@ def search_map(longitude, lattitude, delta):
 
 def get_img_from_response(content):
     image = Image.open(BytesIO(content))
-    print(image.size)
     return image
 
 
@@ -33,16 +32,29 @@ def PIL_to_pygame(img):
     return pygame.image.fromstring(img.tobytes("raw", 'RGB'), img.size, 'RGB')
 
 
-# micro mainloop
-response = search_map(LON, LAT, SCALE)
-pil_image = get_img_from_response(response)
-map_image = PIL_to_pygame(pil_image)
+def get_map():
+    response = search_map(LON, LAT, SCALE)
+    pil_image = get_img_from_response(response)
+    map_image = PIL_to_pygame(pil_image)
+    return map_image
+
+MAP = get_map()
+
 
 while True:
     for e in pygame.event.get():
-        if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
+        if e.type == pygame.QUIT:
             quit()
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_ESCAPE:
+                quit()
+            if e.key == pygame.K_PAGEUP:
+                SCALE += 1
+            if e.key == pygame.K_PAGEDOWN:
+                SCALE -= 1
+            MAP = get_map()
+
     win.fill((0, 0, 0))
-    win.blit(map_image, (0, 0))
+    win.blit(MAP, (0, 0))
     pygame.display.flip()
     clock.tick(rate)
