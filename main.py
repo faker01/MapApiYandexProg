e14f2ce1f2b10a84a59e3a427ac178577758f112
leon-lyceum <57870714+leon-lyceum@users.noreply.github.com>
2020-02-28 17:27:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+
+import pygame
+
+w, h = 800, 600
+rate = 64
+clock = pygame.time.Clock()
+win = pygame.display.set_mode((w, h))
+
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == 27):
+            quit()
+    win.fill((0, 0, 0))
+    pygame.display.flip()
+    clock.tick(rate)
